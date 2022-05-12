5438ccbcfef5665ed799e1a092faa1c00ae9f0b0
cope <ocollinscope@gmail.com>
2022-05-12 09:24:38 +0100

    fixed input form

--- collegeWeb/Scripts/alertContact.js
+++ collegeWeb/Scripts/alertContact.js
@@ -1,8 +1,23 @@
-function checkformEmpty(){
-    console.log("ss")
-    var emailEnter = document.getElementById("emailInput");
-    if(emailEnter.hasAttribute("required") && emailEnter.length == ""){
-        console.log("lol")
-        alert();
+function requiredForm(){
+    var emptyEmail = document.forms["ContactForm"]["Email"].value;
+    var emptyName = document.forms["ContactForm"]["Name"].value;
+    var emptyContent = document.forms["ContactForm"]["ContentArea"].value;
+    if (emptyEmail == ""){
+        alert("Please fill all fields");
+        return false;
+    } 
+    else if (emptyName == ""){
+        alert("Please fill all fields");
+        return false;
     }
+    else if (emptyContent == ""){
+        alert("Please fill all fields");
+        return false;
+    }
+    else{
+        alert("Submitted");
+        return true;
+    }
+
 }
+
